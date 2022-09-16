3c9e75ae3215e15249109c4bb96ad3c59cba22c6
chenshuai615@hellobike.com <chenshuai615@hellobike.com>
2022-09-16 11:01:22 +0800

    完善检索界面

--- src/App.vue
+++ src/App.vue
@@ -1,86 +1,16 @@
 <template>
-  <el-container>
-    <el-header>CBMIR System</el-header>
-    <el-container>
-      <el-aside width="200px">
-        <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
-        >
-          <el-submenu index="0">
-            <template slot="title">
-              <i class="el-icon-house"></i>
-              <span>Home</span>
-            </template>
-          </el-submenu>
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>Lung CT Retrieval</span>
-            </template>
-            <el-menu-item-group>
-              <router-link to="/retrieval/input">
-                <el-menu-item index="1-1">retrieval</el-menu-item>
-              </router-link>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-          </el-submenu>
-        </el-menu>
-      </el-aside>
-      <el-main>
-        <router-view></router-view>
-      </el-main>
-    </el-container>
-  </el-container>
+  <frame></frame>
 </template>
 
 <script>
+  import frame from './components/frame.vue'
 export default {
   name: "App",
-  components: {},
+  components: {
+    frame
+},
 };
 </script>
 <style>
-.el-header,
-.el-footer {
-  background-color: #b3c0d1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
 
-.el-aside {
-  background-color: #d3dce6;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
-}
-
-.el-main {
-  background-color: #e9eef3;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
-}
-
-body > .el-container {
-  margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
-}
-.router-link-active {
-  text-decoration: none;
-}
-a {
-  text-decoration: none;
-}
 </style>
--- src/views/retrieval/input.vue
+++ src/views/retrieval/input.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
+    <!--输入图片-->
     <el-row>
       <el-col>
         <div id="inputBox">
-          <el-upload action="#" list-type="picture-card" :auto-upload="false">
+          <el-upload action="#" list-type="picture-card" :auto-upload="false" :on-change="showResult">
             <i slot="default" class="el-icon-plus"></i>
             <div slot="file" slot-scope="{ file }">
               <img
@@ -41,23 +42,37 @@
         </div>
       </el-col>
     </el-row>
-    <el-row>
+    <!--检索按钮-->
+    <el-row style="margin-top:20px">
+        <el-col span=24>
+            <el-form :inline="true" :model="formInline">
+                <el-form-item label="Please enter the number of images you want to retrieve">
+                    <el-input v-model="searchNum" placeholder="topK"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" icon="el-icon-search">Retrieval</el-button>
+                </el-form-item>
+            </el-form>
+        </el-col>
+    </el-row>
+    <!--检索结果-->
+    <el-row v-show="true" :gutter="20" style="margin-top:20px">
       <el-col
-        :span="8"
-        v-for="(o, index) in 2"
+        :span=4
+        v-for="(o, index) in 10"
         :key="o"
-        :offset="index > 0 ? 2 : 0"
+        :offset="index >= 0 ? 2 : 0"
       >
-        <el-card :body-style="{ padding: '0px' }">
+        <el-card :body-style="{ padding: '0px'}" style="margin-top:10px">
           <img
             src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
             class="image"
           />
           <div style="padding: 14px;height: 10px;">
-            <span>好吃的汉堡</span>
+            <span>Result Case:{{caseNames[index]}}</span>
             <div class="bottom clearfix">
-              <time class="time">{{ currentDate }}</time>
-              <el-button type="text" class="button">操作按钮</el-button>
+              <time class="time">{{currentDate}}</time>
+              <el-button type="text" class="button">Case Info</el-button>
             </div>
           </div>
         </el-card>
@@ -75,8 +90,34 @@ export default {
       dialogImageUrl: "",
       dialogVisible: false,
       disabled: false,
-      currentDate: new Date(),
+      resultShow : false,
+      searchNum: 10,
+      caseNames:[
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10"
+      ]
     };
+  },
+  computed:{
+    currentDate:function(){
+        var now = new Date();                                                                                                     
+		var year = now.getFullYear(); //得到年份
+		var month = now.getMonth();//得到月份
+		var date = now.getDate();//得到日期
+		month = month + 1;
+		if (month < 10) month = "0" + month;
+		if (date < 10) date = "0" + date;
+		var time = year + "-" + month + "-" + date; 
+        return time;
+    }
   },
 
   mounted() {},
@@ -92,6 +133,9 @@ export default {
     handleDownload(file) {
       console.log(file);
     },
+    showResult(){
+        this.resultShow = !this.resultShow
+    }
   },
 };
 </script>
@@ -125,5 +169,9 @@ export default {
 
 .clearfix:after {
   clear: both;
+}
+el-card{
+    height: 50px;
+    width: 50px;
 }
 </style>
